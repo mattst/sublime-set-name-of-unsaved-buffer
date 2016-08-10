ce12d14748c2bfc40d5cf8932e87fd9666b4d3ea
mattst <mattst@i-dig.info>
2016-08-10 21:35:21 +0100

    Removed mark as scratch feature, could lead to loss of work.

--- SetNameOfUnsavedBuffer.py
+++ SetNameOfUnsavedBuffer.py
@@ -21,18 +21,15 @@ def is_sublime_text_3():
 class SetNameOfUnsavedBufferCommand(sublime_plugin.TextCommand):
     """
     A Sublime Text plugin to set the name of an unsaved buffer. The name will
-    be shown in the files tab bar, in the side bar, in the drop-down files
-    list, and in the show files overlay. The purpose of the plugin is so that
-    temporary buffers can be quickly given a name and then switched to easily,
-    this is especially useful if more than one temporary buffer is in use. The
-    plugin can take an optional boolean "set_scratch" arg which will set the
-    buffer's 'scratch' status, this controls whether or not a save prompt will
-    be shown when the buffer is closed, i.e. no save prompt if set to true.
+    be shown in the buffer's tab, in the side bar, in the drop-down files list
+    and in the show files overlay. Its purpose is so that temporary buffers can
+    be conveniently and quickly given a name and then switched to easily, this
+    is especially useful if there are several temporary buffers in use.
     """
 
     def is_enabled(self):
 
-        # Only enables the plugin for unsaved buffers.
+        # The plugin is only enabled for unsaved buffers.
 
         if (self.view.file_name()
          or self.view.is_loading()
@@ -44,24 +41,12 @@ class SetNameOfUnsavedBufferCommand(sublime_plugin.TextCommand):
         return True
 
 
-    def run(self, edit, **kwargs):
-
-        self.handle_set_scratch_setting(**kwargs)
+    def run(self, edit):
 
         msg =  "Enter a name for the unsaved buffer:"
 
         self.view.window().show_input_panel(msg, "",
                            self.on_panel_done, None, None)
-
-
-    def handle_set_scratch_setting(self, **kwargs):
-
-        setting_name = "set_scratch"
-
-        if setting_name in kwargs:
-            setting_value = kwargs.get(setting_name)
-            if isinstance(setting_value, bool):
-                self.view.set_scratch(setting_value)
 
 
     def on_panel_done(self, name_for_buffer):
@@ -75,23 +60,23 @@ class SetNameOfUnsavedBufferCommand(sublime_plugin.TextCommand):
 
         self.view.set_name(name_for_buffer)
 
-        # The bug workaround must be performed by a new TextCommand
-        # because an 'edit' object is required for text insertions,
-        # and this class's 'edit' object expires when run() returns.
+        # The bug workaround must be done by another TextCommand;
+        # an edit object is required and the edit object that was
+        # passed to run() expired as soon as run() returned.
 
         if is_sublime_text_3():
             command_name = "set_name_of_unsaved_buffer_bug_workaround"
-            self.view.window().run_command(command_name)
+            self.view.run_command(command_name)
 
 
 class SetNameOfUnsavedBufferBugWorkaroundCommand(sublime_plugin.TextCommand):
     """
-    There is a bug in Sublime Text 3 when using set_name() to set the name of
-    an unsaved buffer. After assigning the new name it will not be displayed
-    until after the buffer has been modified in some way, i.e. a character is
-    inserted or deleted. This command is a workaround for that bug and it just
-    inserts a space at the end of the buffer and then immediately deletes it.
-    Bug report @: https://github.com/SublimeTextIssues/Core/issues/1180
+    There is a bug in Sublime Text 3 when using set_name(); after it has been
+    called the new name will not be shown anywhere until after the buffer has
+    been modified in some way, i.e. a character is inserted or deleted. This
+    command is a workaround for that bug and it simply inserts a space at the
+    end of the buffer and then immediately deletes it. The bug report can be
+    viewed at this url: https://github.com/SublimeTextIssues/Core/issues/1180
     """
 
     def run(self, edit):
